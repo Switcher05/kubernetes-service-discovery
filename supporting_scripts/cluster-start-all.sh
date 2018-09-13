2803ceea98d142a394d9edf99994e33ef8866942
michaelstievenart <michael.stievenart@bsg.co.za>
2018-09-13 11:27:57 +0200

    vanilla ng2+ app

--- supporting_scripts/cluster-start-all.sh
+++ supporting_scripts/cluster-start-all.sh
@@ -5,18 +5,12 @@ echo "Starting cluster"
 echo "Cluster started"
 
 #making sure role hasn't been previously defined
-kubectl delete clusterrolebindings service-reader-default &>/dev/null
-kubectl delete clusterrolebindings service-reader-k8s &>/dev/null
+kubectl delete clusterrolebindings service-discovery-client &>/dev/null
 
 echo "Creating cluster role"
 apply-all-yamls-with-patches.sh kubernetes-service-discovery .
-kubectl create clusterrolebinding service-reader-k8s \
-  --clusterrole=service-reader  \
-  --serviceaccount=kubernetes-service-discovery:default
+kubectl create rolebinding default:service-discovery-client --clusterrole service-discovery-client --serviceaccount kubernetes-service-discovery:default
 
-kubectl create clusterrolebinding service-reader-default \
-  --clusterrole=service-reader  \
-  --serviceaccount=default:default
 echo "Cluster role created"
 
 echo "Starting UAA"
@@ -34,7 +28,6 @@ echo "API-Gateway Started"
 echo "Starting Ingress"  
 cd nginx
 minikube addons enable ingress
-apply-all-yamls-with-patches.sh nginx-ingress nginx
 echo "$(minikube ip) dev.frontend" | sudo tee -a /etc/hosts
 echo "Ingress Started"  
 echo "Access API-Gateway frontend at http://dev.frontend"
--- supporting_scripts/minikube-start.sh
+++ supporting_scripts/minikube-start.sh
@@ -2,17 +2,23 @@
 
 minikube status | grep "Running" > /dev/null
 if [ $? -eq 0 ]; then
-	echo "Process is running. Applying no action"
+	echo "Minikube is running. Applying no action"
 else
-	echo "Process is not running. Starting Minikube"
+	echo "Minikube is not running. Starting Minikube"
 
 	# checking if platform is mac
 	if [ "$(uname)" == "Darwin" ]; then
 		minikube start --vm-driver hyperkit
 	else
+	    # checking if minikube net is active
+	    virsh net-list --inactive | grep minikube-net > /dev/null
+        if [ $? -eq 0 ]; then
+        echo "Starting minikube-net"
+		virsh net-start minikube-net
+		fi
 		minikube start
 	fi
 
-	kubectl apply -f namespace.yaml
+	#kubectl apply -f namespace.yaml
 	kubectl config set-context minikube --namespace=kubernetes-service-discovery
 fi
